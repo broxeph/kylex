71d98feb874e31e2da5f42f47be97f6712be1c26
alex <alex@ameryn.com>
2018-12-09 00:26:46 -0500

    http -> https; ALLOWED_HOSTS update

--- kylex/settings.py
+++ kylex/settings.py
@@ -55,8 +55,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
-STATICFILES_DIRS = ['kylex/static']
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'kylex/static'),
+)
 
 
 # Email
