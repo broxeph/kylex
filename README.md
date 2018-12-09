1f816e98948b81bcd696db305650ff85c5c077d3
alex <alex@ameryn.com>
2018-12-09 00:55:14 -0500

    Use whitenoise for static file serving with Heroku

---
+++ README.md
@@ -0,0 +1,8 @@
+kylex.name
+==========
+
+Built with Django for email sending functionality :)
+
+Most Django functionality was ripped out.
+
+Static files served with [whitenoise](http://whitenoise.evans.io/en/stable/#infrequently-asked-questions) since S3 would be overkill.
--- kylex/settings.py
+++ kylex/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -55,7 +56,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Email
