f5bc9f3165d66af72e65b5cbaec62b04b649e55f
alex <alex@ameryn.com>
2018-12-09 00:08:27 -0500

    DEBUG, ALLOWED_HOSTS

--- kylex/settings.py
+++ kylex/settings.py
@@ -3,15 +3,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+# Allow all host headers
+ALLOWED_HOSTS = ['kylex-wedding.herokuapp.com', 'kylex.name']
 
 
 # Application definition
