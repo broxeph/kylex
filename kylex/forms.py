b8bb98180a400807fc11196f6c19fe73d507863e
alex <alex@ameryn.com>
2018-12-08 23:43:01 -0500

    EmailField max_length

--- kylex/forms.py
+++ kylex/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
-    email = forms.EmailField()
+    email = forms.EmailField(max_length=100)
     comments = forms.CharField(max_length=1000)
--- kylex/settings.py
+++ kylex/settings.py
@@ -64,10 +64,10 @@ STATICFILES_DIRS = ['kylex/static']
 
 # Email
 
-EMAIL_USE_TLS = True
 FROM_EMAIL = 'noreply@kylex.name'
 TO_EMAIL = 'alex.kylie.8@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = TO_EMAIL
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
