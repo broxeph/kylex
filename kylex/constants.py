21a8a83452211117c721756c649201eb3a5d0024
alex <alex@ameryn.com>
2019-01-05 16:11:01 -0500

    Initial batch of bridal party photos

--- kylex/constants.py
+++ kylex/constants.py
@@ -3,39 +3,40 @@ Data that was too unwieldy for views.py.
 
 If Heroku supported Python 3.7, we could put these in DataClasses.
 """
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 GALLERY = ['dancing', 'kitchen', 'bibou', 'engagement', 'one-water', 'deb', 'sexpod']
 
 BRIDESMAIDS = [
     {
         'name': 'Ali McCulla',
-        'img': 'https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/19956696_10155561403253552_8063942698576058187_o.jpg?_nc_cat=104&_nc_ht=scontent-lga3-1.xx&oh=98c8b32c134712bbc09125c35f139ac7&oe=5C9F5FF1',
+        'img': static('img/ali.jpg'),
         'title': 'Maid of Honor',
-        'story': 'This person is a riddle, wrapped in a mystery, inside an enigma; but perhaps there is a key.',
+        'story': '',
     },
     {
-        'name': 'Ellen Pokorny',
-        'img': 'https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/26172626_10157083326492796_1251368699509558302_o.jpg?_nc_cat=103&_nc_ht=scontent-lga3-1.xx&oh=39fa860d05366857040cb96701425c1a&oe=5CB20F03',
+        'name': 'Ellen Boyer Pokorny',
+        'img': static('img/ellen.jpg'),
         'title': 'Bridesmaid',
-        'story': 'This person is a riddle, wrapped in a mystery, inside an enigma; but perhaps there is a key.',
+        'story': '',
     },
     {
         'name': 'Emily Maple',
         'img': 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/43567979_10214687533674823_5410537132185878528_n.jpg?_nc_cat=107&_nc_ht=scontent-lga3-1.xx&oh=f3df2586096de9b948bbe7722c7c9b57&oe=5C8F1C84',
         'title': 'Bridesmaid',
-        'story': 'This person is a riddle, wrapped in a mystery, inside an enigma; but perhaps there is a key.',
+        'story': '',
     },
     {
         'name': 'Lauren Ball',
         'img': 'https://scontent-lga3-1.xx.fbcdn.net/v/t31.0-8/27500398_1863915210345197_3134240069504316129_o.jpg?_nc_cat=111&_nc_ht=scontent-lga3-1.xx&oh=c99b30f6f24cb135b1ebd1d336b58ef6&oe=5CAD62FB',
         'title': 'Bridesmaid',
-        'story': 'This person is a riddle, wrapped in a mystery, inside an enigma; but perhaps there is a key.',
+        'story': '',
     },
     {
         'name': 'Lindsay Cutsler',
-        'img': 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/47267394_10217598456901141_9210513918382833664_o.jpg?_nc_cat=110&_nc_ht=scontent-lga3-1.xx&oh=099c5e3624eeecd7dcb2e058479c0ed2&oe=5C9F1E97',
+        'img': static('img/lindsay.jpg'),
         'title': 'Bridesmaid',
-        'story': 'This person is a riddle, wrapped in a mystery, inside an enigma; but perhaps there is a key.',
+        'story': '',
     },
 ]
 
@@ -44,30 +45,30 @@ GROOMSMEN = [
         'name': 'Justin Ball',
         'img': 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/40576635_2343442069005052_408757148571402240_o.jpg?_nc_cat=101&_nc_ht=scontent-lga3-1.xx&oh=16517a946e5815561b97988d81a185d5&oe=5C9C0B91',
         'title': 'Best Man',
-        'story': 'This person is a riddle, wrapped in a mystery, inside an enigma; but perhaps there is a key.',
+        'story': '',
     },
     {
         'name': 'Ben Glenn',
         'img': 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/28059052_10213733029854860_8151471974392820739_n.jpg?_nc_cat=106&_nc_ht=scontent-lga3-1.xx&oh=b74e3c6b1c9d4152e30f747efd90dd8b&oe=5C9586A5',
         'title': 'Groomsman',
-        'story': 'This person is a riddle, wrapped in a mystery, inside an enigma; but perhaps there is a key.',
+        'story': '',
     },
     {
         'name': 'Frankie Maple',
         'img': 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/15032096_10153840577810946_1198070499810869110_n.jpg?_nc_cat=111&_nc_ht=scontent-lga3-1.xx&oh=100dbf082c27cab101f08bfde9b5dfc9&oe=5CA55733',
         'title': 'Groomsman',
-        'story': 'This person is a riddle, wrapped in a mystery, inside an enigma; but perhaps there is a key.',
+        'story': '',
     },
     {
         'name': 'Kyle Cullison',
         'img': 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/34881920_10156508335812082_7825779516843753472_n.jpg?_nc_cat=105&_nc_ht=scontent-lga3-1.xx&oh=41f83036cb85186e97aaafa0f9150182&oe=5CA5C3CF',
         'title': 'Groomsman',
-        'story': 'This person is a riddle, wrapped in a mystery, inside an enigma; but perhaps there is a key.',
+        'story': '',
     },
     {
         'name': 'Michael Pokorny',
-        'img': 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/43207535_10155467529755672_4622414086523584512_n.jpg?_nc_cat=102&_nc_ht=scontent-lga3-1.xx&oh=974dd562c80f7075499bfe1229bcabf0&oe=5CA1B5BF',
+        'img': static('img/mick.jpg'),
         'title': 'Groomsman',
-        'story': 'This person is a riddle, wrapped in a mystery, inside an enigma; but perhaps there is a key.',
+        'story': '',
     },
 ]
