bbe8c212789a3bad52f26b832d6c9c8d41ad345d
Alex Ball <alex@ball.lol>
2019-06-18 23:25:23 -0400

    Upgrade Django to 2.2; upgrade email backend from TSL to SSL

--- kylex/settings.py
+++ kylex/settings.py
@@ -100,7 +100,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'alex.kylie.8@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
